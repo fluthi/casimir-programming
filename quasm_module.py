d6622bb9f428de600a053646f7cb6483f73cce01
Florian Luthi <f.luthi@tudelft.nl>
2016-10-27 14:10:34 +0000

    single qubit

--- quasm_module.py
+++ quasm_module.py
@@ -18,10 +18,9 @@ def get_filtered_qasm(list_of_qasm_strings):
     Return: list of strings without comments
     '''
     quasm_instrunctions=[]
-    alphabet='abcdefghijklmnopqrstuvwxyz'
     m=0
     for tt, line in enumerate(list_of_qasm_strings):
-        if not line[0] in alphabet:
+        if line[0]=='#':
             m+=1
             pass
         else:
@@ -45,13 +44,13 @@ def number_of_qubits(quasm_instrunctions):
             num_qubits+=1
     return num_qubits
 
-def delete_instruction(instructions,number_to_be_deleted):
+def delete_instruction(instructions,number_to_be_deleted=1):
     """
     In: list of instructions and number of instructions to remove
     ---
     Out: list with removed instructions (from top)
     """
-    return instructions[number_to_be_deleted-1:len(instructions)-1]
+    return instructions[number_to_be_deleted:len(instructions)-1]
 
 def read_instruction_line(instructions):
     """
@@ -68,3 +67,21 @@ def read_instruction_line(instructions):
         qubits = instruction[1].split(",")   
     return [operator, [qubits]]
         
+def single_qubit_gate(gate,qubit_number,number_of_qubits):
+    '''
+    In: which gate is to be applied, qubit_number: which qubit is acted on (first qubit is 0), number_of_qubits: total number of qubits
+    ---
+    Out: Process matrix with dimension 2**number_of_qubits x 2**number_of_qubits
+    '''
+    gate_dict=operator_dict_default()
+    single_qubit_gate=gate_dict['gate']
+    if qubit_number==0:
+        matrix=single_qubit_gate
+    else:
+        matrix=gate_dict['i']
+    for tt in range(1,len(number_of_qubits)):
+        if tt==qubit_number:
+            matrix=np.outer(matrix,single_qubit_gate)
+        else:
+            matrix=np.outer(matrix,gate_dict['i'])
+    return matrix
